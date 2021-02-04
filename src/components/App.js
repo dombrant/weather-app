29704c24e200f5b4d90a8af5f0f40c7906351713
Dominic Brant <dombrant@gmail.com>
2021-02-04 15:18:37 -0800

    set up state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ForecastItem from "./ForecastItem";
 import days from "../utilities/getDays";
 
 const App = () => {
+  const [location, setLocation] = useState([]);
+  // Set forecast to five "Loadings" to start
+  // It will later change to forecast for the next five days
+  const [forecast, setForecast] = useState([
+    "Loading",
+    "Loading",
+    "Loading",
+    "Loading",
+    "Loading",
+  ]);
+
+  // Make a request to the API whenever the location is updated.
+  useEffect(() => {}, [location]);
+
   return (
     <div>
       <Header />
